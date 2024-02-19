5f4f2e6e72cb08a7741716d5b4135ddd23f90fd4
Abdul Ghani <ghania99s.ag@gmail.com>
2024-02-19 11:13:16 +0700

    change path in navbar

--- src/components/header.js
+++ src/components/header.js
@@ -6,11 +6,11 @@ function Header() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Inersia</Navbar.Brand>
+        <Navbar.Brand href="/">Inersia</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#services">Services</Nav.Link>
             <Nav.Link href="#works">Works</Nav.Link>
