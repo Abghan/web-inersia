28b75ceb321bed3870ff19c631f3b0d8132aecae
Abdul Ghani <ghania99s.ag@gmail.com>
2024-02-19 15:17:58 +0700

    test some nav funtion

--- src/App.js
+++ src/App.js
@@ -4,24 +4,12 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { Header, Footer } from './components';
 import { Home, AboutPage, LoginPage, NotFound } from './pages';
-import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header id="header">
-        {/* <Header /> */}
-        <nav>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/works">Works</Link>
-          </li>
-        </nav>
+        <Header />
       </header>
       <main className="hero-block">
         <Routes>
--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,33 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/">Inersia</Navbar.Brand>
+        <Navbar.Brand>
+          <Link to="/">Inersia</Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link linkTo="/">Home</Nav.Link>
-            <Nav.Link to="/about">About</Nav.Link>
-            <Nav.Link to="/services">Services</Nav.Link>
-            <Nav.Link to="/works">Works</Nav.Link>
-            {/* <Nav.Link href="/teams">Teams</Nav.Link>
-            {/* // <Nav.Link href="/testimonials">Testimonials</Nav.Link> */}
-            {/* // <Nav.Link href="/pricing">Pricing</Nav.Link> */}
-            {/* // <Nav.Link href="/blog">Blog</Nav.Link> */}
-            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link>
+              <Link to="/">Home</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/about">About</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/services">Services</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/services">Works</Link>
+            </Nav.Link>
+            {/* <Nav.Link href="/teams">Teams</Nav.Link> */}
+
+            <Nav>Contact</Nav>
           </Nav>
         </Navbar.Collapse>
       </Container>
