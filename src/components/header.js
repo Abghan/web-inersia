6800984f757dad46654a407a8e3c31dacccd14f4
Abdul Ghani <ghania99s.ag@gmail.com>
2024-02-20 11:01:52 +0700

    change some style for nav-link

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function Header() {
   return (
@@ -13,21 +13,21 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link>
-              <Link to="/">Home</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/about">About</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/services">Services</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/services">Works</Link>
-            </Nav.Link>
-            {/* <Nav.Link href="/teams">Teams</Nav.Link> */}
-
-            <Nav>Contact</Nav>
+            <NavLink className="nav-link" to="/">
+              Home
+            </NavLink>
+            <NavLink className="nav-link" to="/about">
+              About
+            </NavLink>
+            <NavLink className="nav-link" to="/services">
+              Services
+            </NavLink>
+            <NavLink className="nav-link" to="/works">
+              Works
+            </NavLink>
+            <NavLink className="nav-link" to="/contact">
+              Contact
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
